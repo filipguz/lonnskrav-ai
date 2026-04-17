8c3544fc884ce716dbabe926078960834c455f56
filipguz <filip@gustavsenfriluft.no>
2026-04-17 14:20:37 +0200

    test

---
+++ src/main/java/sjogang/lonnskrav/LonnskravApplication.java
@@ -0,0 +1,13 @@
+package sjogang.lonnskrav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LonnskravApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LonnskravApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sjogang/lonnskrav/LonnskravApplicationTests.java
@@ -0,0 +1,13 @@
+package sjogang.lonnskrav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LonnskravApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
